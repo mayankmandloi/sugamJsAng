d212d19bc620e1ddd7a4e78e51fa2a4b87ea9c71
mayankmandloi <mayankmandloi009@gmail.com>
2017-08-28 20:40:12 +0530

    .

---
+++ AJAX/test.js
@@ -0,0 +1,18 @@
+var cors = require('cors')
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello I am Sugam from Web Bhumi\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
